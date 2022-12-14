a0c80e0a887da9e76b545f1ab70d52793b90bc0b
Mhayk Whandson <hi@mhayk.com>
2022-12-14 14:47:55 +0000

    feat: configure prisma service and configure the controller to list and create notifications

--- README.md
+++ README.md
@@ -6,3 +6,13 @@ $ npm i prisma -D
 $ npm i @prisma/client
 $ npx prisma init --datasource-provider SQLite
 ```
+
+### Migrations
+```bash
+$ npx prisma migrate dev
+```
+
+### Prisma Studio
+```bash
+$ npx prisma studio
+```
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma.service';
+import { randomUUID } from 'node:crypto';
 
-@Controller()
+@Controller('notifications')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly prisma: PrismaService,
+  ) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  list() {
+    return this.prisma.notification.findMany();
+  }
+
+  @Post()
+  async create() {
+    return this.prisma.notification.create({
+      data: {
+        id: randomUUID(),
+        content: 'You have a new friend request!',
+        category: 'social',
+        recipientId: randomUUID(),
+      },
+    });
   }
 }
