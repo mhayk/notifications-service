5ccecafe1eb2e5707520b594b05be55bce08959d
Mhayk Whandson <hi@mhayk.com>
2022-12-15 16:50:34 +0000

    test: create an unit test for notification repository

--- src/application/use-cases/send-notification.spec.ts
+++ src/application/use-cases/send-notification.spec.ts
@@ -1,16 +1,25 @@
 import { randomUUID } from 'crypto';
+import { Notification } from '../entities/notification';
 import { SendNotification } from './send-notification';
+
+const notifications: Notification[] = [];
+
+const notificationRepository = {
+  async create(notification: Notification) {
+    notifications.push(notification);
+  },
+};
 
 describe('Send notification', () => {
   it('should be able to send a notification', async () => {
-    const sendNotification = new SendNotification();
+    const sendNotification = new SendNotification(notificationRepository);
 
-    const { notification } = await sendNotification.execute({
+    await sendNotification.execute({
       recipientId: randomUUID(),
       content: 'This is a notification',
       category: 'social',
     });
 
-    expect(notification).toBeTruthy();
+    expect(notifications).toHaveLength(1);
   });
 });
