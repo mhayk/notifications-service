603d3d935b1e97bcf4a387b92664e70616e995a1
Mhayk Whandson <hi@mhayk.com>
2022-12-15 14:41:19 +0000

    test: group the unit tests

--- src/application/entities/content.spec.ts
+++ src/application/entities/content.spec.ts
@@ -1,15 +1,17 @@
 import { Content } from './content';
 
-test('it should be able to create a notification content', () => {
-  const content = new Content('You have received a friend request');
+describe('Notification content', () => {
+  it('should be able to create a notification content', () => {
+    const content = new Content('You have received a friend request');
 
-  expect(content).toBeTruthy();
-});
+    expect(content).toBeTruthy();
+  });
 
-test('it should not be able to create a notification content with less then 5 characters', () => {
-  expect(() => new Content('aaa')).toThrowError();
-});
+  it('should not be able to create a notification content with less then 5 characters', () => {
+    expect(() => new Content('aaa')).toThrowError();
+  });
 
-test('it should not be able to create a notification content with more then 240 characters', () => {
-  expect(() => new Content('a'.repeat(241))).toThrowError();
+  it('should not be able to create a notification content with more then 240 characters', () => {
+    expect(() => new Content('a'.repeat(241))).toThrowError();
+  });
 });
