cd95b164ae2dc0c2e4311199b83bd5fb45758a8b
Mhayk Whandson <hi@mhayk.com>
2022-12-21 09:05:36 +0000

    feat: create two new required methods findById and save

--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -4,6 +4,14 @@ import { NotificationsRepository } from '@application/repositories/notifications
 export class InMemoryNotificationRepository implements NotificationsRepository {
   public notifications: Notification[] = [];
 
+  async findById(notificationId: string): Promise<Notification | null> {
+    throw new Error('Method not implemented.');
+  }
+
+  async save(notification: Notification): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+
   async create(notification: Notification) {
     this.notifications.push(notification);
   }
