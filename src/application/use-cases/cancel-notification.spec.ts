53e3583884990525d5f07a5894d585e7267b923e
Mhayk Whandson <hi@mhayk.com>
2022-12-21 11:22:04 +0000

    test: create the unit test for the use case cancel notification

---
+++ src/application/use-cases/cancel-notification.spec.ts
@@ -0,0 +1,39 @@
+import { InMemoryNotificationRepository } from '@test/repositories/in-memory-notifications-repository';
+import { CancelNotification } from './cancel-notification';
+import { Notification } from '@application/entities/notification';
+import { Content } from '@application/entities/content';
+import { NotificationNotFound } from './errors/notification-not-found';
+
+describe('Cancel notification', () => {
+  it('should be able to cancel a notification', async () => {
+    const notificationRepository = new InMemoryNotificationRepository();
+    const cancelNotification = new CancelNotification(notificationRepository);
+
+    const notification = new Notification({
+      content: new Content('New friend request!'),
+      category: 'social',
+      recipientId: 'example-recipient-id',
+    });
+
+    await notificationRepository.create(notification);
+
+    await cancelNotification.execute({
+      notificationId: notification.id,
+    });
+
+    expect(notificationRepository.notifications[0].canceledAt).toEqual(
+      expect.any(Date),
+    );
+  });
+
+  it('should not be able to cancel a non existing notification', () => {
+    const notificationRepository = new InMemoryNotificationRepository();
+    const cancelNotification = new CancelNotification(notificationRepository);
+
+    expect(() => {
+      return cancelNotification.execute({
+        notificationId: 'non-existing-id',
+      });
+    }).rejects.toThrowError(NotificationNotFound);
+  });
+});
