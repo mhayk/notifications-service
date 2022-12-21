362d892ffc9c6acd2c6c69e001596ecd2ece512d
Mhayk Whandson <hi@mhayk.com>
2022-12-21 16:08:43 +0000

    test: create the unit test to get notifications by recipient id

---
+++ src/application/use-cases/get-recipient-notifications.spec.ts
@@ -0,0 +1,44 @@
+import { Content } from '@application/entities/content';
+import { Notification } from '@application/entities/notification';
+import { makeNotification } from '@test/factories/notification-factory';
+import { InMemoryNotificationRepository } from '@test/repositories/in-memory-notifications-repository';
+import { GetRecipientNotifications } from './get-recipient-notifications';
+
+describe('Count recipient notifications', () => {
+  it('should be able to get recipient notifications', async () => {
+    const notificationsRepository = new InMemoryNotificationRepository();
+    const getRecipientNotifications = new GetRecipientNotifications(
+      notificationsRepository,
+    );
+
+    await notificationsRepository.create(
+      makeNotification({
+        recipientId: 'recipient-id-1',
+      }),
+    );
+
+    await notificationsRepository.create(
+      makeNotification({
+        recipientId: 'recipient-id-1',
+      }),
+    );
+
+    await notificationsRepository.create(
+      makeNotification({
+        recipientId: 'recipient-id-2',
+      }),
+    );
+
+    const { notifications } = await getRecipientNotifications.execute({
+      recipientId: 'recipient-id-1',
+    });
+
+    expect(notifications).toHaveLength(2);
+    expect(notifications).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({ recipientId: 'recipient-id-1' }),
+        expect.objectContaining({ recipientId: 'recipient-id-1' }),
+      ]),
+    );
+  });
+});
