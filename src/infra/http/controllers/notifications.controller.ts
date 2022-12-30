04b7ce9056037045859cadc61d6b6ec7f8f19430
Mhayk Whandson <hi@mhayk.com>
2022-12-30 18:07:47 +0000

    feat: create a new endpoint '/notifications/:id/cancel'

--- src/infra/http/controllers/notifications.controller.ts
+++ src/infra/http/controllers/notifications.controller.ts
@@ -1,11 +1,22 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { SendNotification } from '@application/use-cases/send-notification';
 import { CreateNotificationBody } from '../dtos/create-nofitication-body';
 import { NotificationViewModel } from '../view-models/notification-view-model';
+import { CancelNotification } from '@application/use-cases/cancel-notification';
 
 @Controller('notifications')
 export class NotificationsController {
-  constructor(private sendNotification: SendNotification) {}
+  constructor(
+    private sendNotification: SendNotification,
+    private cancelNotification: CancelNotification,
+  ) {}
+
+  @Patch(':id/cancel')
+  async cancel(@Param('id') id: string) {
+    await this.cancelNotification.execute({
+      notificationId: id,
+    });
+  }
 
   @Post()
   async create(@Body() body: CreateNotificationBody) {
