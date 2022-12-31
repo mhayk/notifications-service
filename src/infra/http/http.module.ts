bfee7cc9d98aab62c7cfe8d0a1651a70c6be6d76
Mhayk Whandson <hi@mhayk.com>
2022-12-31 11:26:06 +0000

    feat: import the use case Read Notification in the http module

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -3,10 +3,11 @@ import { SendNotification } from '@application/use-cases/send-notification';
 import { DatabaseModule } from '../database/database.module';
 import { NotificationsController } from './controllers/notifications.controller';
 import { CancelNotification } from '@application/use-cases/cancel-notification';
+import { ReadNotification } from '@application/use-cases/read-notification';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [NotificationsController],
-  providers: [SendNotification, CancelNotification],
+  providers: [SendNotification, CancelNotification, ReadNotification],
 })
 export class HttpModule {}
