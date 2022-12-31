83e243c5c2523e04b9c7bb1c4c738d87dd308ffc
Mhayk Whandson <hi@mhayk.com>
2022-12-31 11:04:24 +0000

    feat: import the use case Cancel Notification in the http module.

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { SendNotification } from '@application/use-cases/send-notification';
 import { DatabaseModule } from '../database/database.module';
 import { NotificationsController } from './controllers/notifications.controller';
+import { CancelNotification } from '@application/use-cases/cancel-notification';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [NotificationsController],
-  providers: [SendNotification],
+  providers: [SendNotification, CancelNotification],
 })
 export class HttpModule {}
