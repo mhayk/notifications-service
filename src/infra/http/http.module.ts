398ba438654d4626c14fee1bf45d89262d684dba
Mhayk Whandson <hi@mhayk.com>
2022-12-31 11:29:40 +0000

    feat: import the use case Unread Notification in the http module

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -4,10 +4,16 @@ import { DatabaseModule } from '../database/database.module';
 import { NotificationsController } from './controllers/notifications.controller';
 import { CancelNotification } from '@application/use-cases/cancel-notification';
 import { ReadNotification } from '@application/use-cases/read-notification';
+import { UnreadNotification } from '@application/use-cases/unread-notification';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [NotificationsController],
-  providers: [SendNotification, CancelNotification, ReadNotification],
+  providers: [
+    SendNotification,
+    CancelNotification,
+    ReadNotification,
+    UnreadNotification,
+  ],
 })
 export class HttpModule {}
